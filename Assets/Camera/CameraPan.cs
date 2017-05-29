2806de89298618bac34d065d8ef91d6e828a51b4
Li Gangwei <michele@n1164845233.netvigator.com>
2017-05-29 13:32:05 +0800

    Finished Twin Sticks

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPan : MonoBehaviour {
+	GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		if(!(ball = GameObject.FindGameObjectWithTag("Player"))) {
+			Debug.LogError("Ball not found!");
+		}
+	}
+
+	void LateUpdate() {
+		transform.LookAt(ball.transform);
+	}
+}
---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour {
+	GameObject ball;
+	private Vector3 armRotation;
+
+	// Use this for initialization
+	void Start () {
+		if(!(ball = GameObject.FindGameObjectWithTag("Player"))) {
+			Debug.LogError("Ball not found!");
+		}
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = ball.transform.position;
+
+		armRotation.y += CrossPlatformInputManager.GetAxis("RHorizontal");
+		armRotation.x += CrossPlatformInputManager.GetAxis("RVertical");
+
+		transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
